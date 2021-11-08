e6f06ce15503092654ed1ede688740b0dc8e29c6
Hrithika-22 <hrithika.adla@gmail.com>
2021-11-07 21:41:24 -0500

    Final changes for wp2

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,8 +1,8 @@
 
 function plusOne(){
   document.getElementById("quan1").value = parseInt(document.getElementById("quan1").value) + 1;
-  calT("total1","quan1");
-  plusSubTot("quan1");
+  calT("total1","quan1")
+  plusSubTot("quan1")
 }
 
 function minusOne(){
@@ -10,14 +10,14 @@ function minusOne(){
   if (val >= 1){
     document.getElementById("quan1").value = val - 1;
   }
-  calT("total1","quan1");
-  minusSubTot("quan1");
+  calT("total1","quan1")
+  minusSubTot("quan1")
 }
 
 function plusOne1(){
   document.getElementById("quan2").value = parseInt(document.getElementById("quan2").value) + 1;
-  calT("total2","quan2");
-  plusSubTot("quan2");
+  calT("total2","quan2")
+  plusSubTot("quan2")
 }
 
 function minusOne1(){
@@ -25,14 +25,14 @@ function minusOne1(){
   if (val >= 1){
     document.getElementById("quan2").value = val - 1;
   }
-  calT("total2","quan2");
-  minusSubTot("quan2");
+  calT("total2","quan2")
+  minusSubTot("quan2")
 }
 
 function plusOne2(){
   document.getElementById("quan3").value = parseInt(document.getElementById("quan3").value) + 1;
-  calT("total3","quan3");
-  plusSubTot("quan3");
+  calT("total3","quan3")
+  plusSubTot("quan3")
 }
 
 function minusOne2(){
@@ -40,28 +40,28 @@ function minusOne2(){
   if (val >= 1){
     document.getElementById("quan3").value = val - 1;
   }
-  calT("total3","quan3");
-  minusSubTot("quan3");
+  calT("total3","quan3")
+  minusSubTot("quan3")
 }
 
 function plusSubTot(q){
   document.getElementById("subT").innerHTML = 5 + parseInt(document.getElementById(q).value)*5;
-  calTax();
+  calTax()
 }
 function minusSubTot(q){
   document.getElementById("subT").innerHTML = 5 - parseInt(document.getElementById(q).value)*5;
-  calTax();
+  calTax()
 }
 
 function calTot(){
   document.getElementById("total").innerHTML = parseFloat(document.getElementById("subT").innerHTML) +
-                                              parseFloat(document.getElementById("tax").innerHTML) +5;
+                                              parseFloat(document.getElementById("tax").innerHTML) + 5;
 }
 function calTax(){
   document.getElementById("tax").innerHTML = parseFloat(document.getElementById("subT").innerHTML)*0.15;
-  calTot();
+  calTot()
 }
 
 function calT(t,q){
-  document.getElementById(t).innerHTML = "$" + parseInt(document.getElementById(q).value) * 5;
+  document.getElementById(t).innerHTML = parseFloat(document.getElementById(q).value) * 5;
 }
